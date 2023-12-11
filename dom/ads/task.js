91c1764deaa40d3daa9c6f5fbdc71a752a8b3c70
Roman Rogschev <omuch13rus@rambler.ru>
2023-12-11 19:55:00 +0200

    dell comment

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -3,11 +3,11 @@ const rotatorCase = Array.from(document.querySelectorAll('.rotator__case'))
 
 let i = 0
 
-let id = setTimeout(function rotate() { // запускаем наш stTimeout с интервалом 500мл
+let id = setTimeout(function rotate() { 
     rotatorCase[i % rotatorCase.length].classList.remove('rotator__case_active')
     rotatorCase[(i + 1) % rotatorCase.length].classList.add('rotator__case_active')
     rotatorCase[(i + 1) % rotatorCase.length].style.color = rotatorCase[(i + 1) % rotatorCase.length].dataset.color;
     let speed = rotatorCase[(i + 1) % rotatorCase.length].dataset.speed
-    i += 1; // тут выполняются действия
-    id = setTimeout(rotate, speed); // замещаем id новым setTimeout который ждет когда выполнится код выше, ждет пол секунды и опять выполняет функцию log
+    i += 1; 
+    id = setTimeout(rotate, speed); 
 }, 100);
