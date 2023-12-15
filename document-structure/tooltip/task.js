f03f53993216258ce555d293f090d3b8c36785a9
Roman Rogschev <omuch13rus@rambler.ru>
2023-12-15 15:09:23 +0200

    modifided task 1 tool

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,21 @@
 const tooltip = Array.from(document.querySelectorAll('.has-tooltip'));
 
 tooltip.forEach(function(tool) {
+    let div = document.createElement('div');
+    div.className = "tooltip";
+    div.textContent = tool.title;
+    tool.insertAdjacentElement("afterend", div);
     tool.addEventListener('click', (e) => {
-        
         e.preventDefault();
-        const toolold = Array.from(document.querySelectorAll('.tooltip'))
-        for (let i of toolold) {
-            if (i.classList.contains('tooltip_active')) {
-                i.classList.remove('tooltip_active')
-            }
+        let toolold = Array.from(document.querySelectorAll('.tooltip'));
+        let cart = toolold.find(item => item.classList.contains('tooltip_active'));
+        if (cart) {
+            cart.classList.remove('tooltip_active');
+        } else {
+            let div = e.target.nextElementSibling
+            const {left} = e.target.getBoundingClientRect();
+            div.style.left = left + 'px'
+            div.classList.add('tooltip_active');
         }
-        const {left} = e.target.getBoundingClientRect();
-        let div = document.createElement('div');
-        div.className = "tooltip";
-        div.textContent = tool.title;
-        div.style.left = left + 'px'; 
-        tool.insertAdjacentElement("afterend", div);
-        div.classList.add('tooltip_active');
     });
 });
